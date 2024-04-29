82869265c810feb795302e3752ed24822513dd60
Koushik Dey <kousickdey3@gmail.com>
2024-04-29 23:40:05 +0530

    RMX2020: VoltageOS Bringup!
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Change-Id: Idfb03202e68ca7737544cdf6d59f2531761ecbab

--- voltage_RMX2020.mk
+++ voltage_RMX2020.mk
@@ -12,11 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common ProjectElixir stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_RMX2020
+PRODUCT_NAME := voltage_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2020
@@ -26,14 +26,17 @@ PRODUCT_SYSTEM_MODEL := RMX2020
 PRODUCT_SYSTEM_NAME := RMX2020
 PRODUCT_SYSTEM_DEVICE := RMX2020
 
-# Project-Elixir additions
-IS_PHONE := true
-ELIXIR_BUILD_TYPE := OFFICIAL
+# VoltageOS additions
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Build info
+BUILD_FINGERPRINT := "realme/RMX2020/RMX2020:11/RP1A.200720.011/1652239672541:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+    TARGET_DEVICE=RMX2020 \
+    PRODUCT_NAME=RMX2020 \
+    PRIVATE_BUILD_DESC="RMX2020-user 11 RP1A.200720.011 1652239672541 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
